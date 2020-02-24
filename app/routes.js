61ad2149aef22862a66ddd66d423f3b1b6b9c032
trevorsaint <hello@trevorsaint.uk>
2020-02-24 11:36:20 +0000

    Added results page with some basic functionality based on selection (WIP)

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./views/transition-check/routes'));
+
 module.exports = router;
---
+++ app/views/transition-check/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+// router.get('/transition-check', (req, res) => {
+//   res.render('transition-check');
+// });
+
+
+module.exports = router;
